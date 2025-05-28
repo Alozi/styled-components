12623964bee995eb7b66cf215b158595e2498a8e
Alozi <alozimozi@gmail.com>
2025-05-28 13:44:32 +0300

    Install library styled components and reset code with library modern-css-reset

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,25 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import styledComponentsLogo from "./assets/styled-components.svg";
+
+import { GlobalStyle } from "./styles/GlobalStyle.js";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
+      <GlobalStyle />
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
+        <a href="https://styled-components.com/" target="_blank">
+          <img
+            src={styledComponentsLogo}
+            className="logo styled components library"
+            alt="Styled components library logo"
+            height={200}
+            width={200}
+          />
         </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <h1>Styled components pet project</h1>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/styles/GlobalStyle.js
@@ -0,0 +1,10 @@
+import { createGlobalStyle } from 'styled-components';
+import 'modern-css-reset';
+
+export const GlobalStyle = createGlobalStyle`
+  body {
+    font-family: sans-serif;
+    background-color: #fff;
+    color: #333;
+  }
+`;
