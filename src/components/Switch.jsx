5d51690aeb9c32cf5091bd88a82d83ff6e9d4823
Alozi <alozimozi@gmail.com>
2025-05-30 17:19:04 +0300

    Realize toogle logic with syled-components and theme

--- src/components/Switch.jsx
+++ src/components/Switch.jsx
@@ -1,16 +1,9 @@
-import { useState } from "react";
 import "./Switch.css";
 
-export default function Switch() {
-  const [isToggled, setIsToggled] = useState(false);
-
-  const onToggle = () => {
-    setIsToggled(!isToggled);
-  };
-
+export default function Switch({ isDarkTheme, onToggle }) {
   return (
     <label className="toggle-switch">
-      <input type="checkbox" checked={isToggled} onChange={onToggle} />
+      <input type="checkbox" checked={isDarkTheme} onChange={onToggle} />
       <span className="switch" />
     </label>
   );
--- src/pages/ThemeToogle.jsx
+++ src/pages/ThemeToogle.jsx
@@ -1,21 +1,71 @@
+import styled, { ThemeProvider } from "styled-components";
+
 import MoonIcon from "../components/icons/MoonIcon";
 import SunIcon from "../components/icons/SunIcon";
 import Switch from "../components/Switch";
+import { useState } from "react";
 
-const style = {
-  minHeight: "100vh",
-  textAlign: "center",
-  paddingTop: "10rem",
-  backgroundColor: "#f5f5f5",
+const StyleApp = styled.div`
+  min-height: 100vh;
+  text-align: center;
+  padding-top: 10rem;
+  background-color: ${(props) => props.theme.body};
+`;
+
+const Name = styled.h1`
+  margin: 1rem;
+  color: ${(props) => props.theme.title};
+`;
+
+const Info = styled.p`
+  margin: 1rem;
+  color: ${(props) => props.theme.subtitle};
+`;
+
+const Link = styled.a`
+  color: ${(props) => props.theme.subtitle};
+
+  &:hover {
+    color: #bf4f74;
+  }
+`;
+
+const lightTheme = {
+  body: "#fff",
+  title: "#1c1c1c",
+  subtitle: "#333",
+  icon: "#1c1c1c",
+};
+
+const darkTheme = {
+  body: "#1c1c1c",
+  title: "#fff",
+  subtitle: "#b6b6b6",
+  icon: "#fff",
 };
 
 export default function ThemeToogle() {
+  const [isDarkTheme, setIsDarkTheme] = useState(false);
+
+  function onToggle() {
+    setIsDarkTheme((prev) => !prev);
+  }
+
   return (
-    <div style={style}>
-      <SunIcon />
-      <Switch />
-      <MoonIcon />
-      <h1>Victoria Bogustka</h1>
-    </div>
+    <ThemeProvider theme={isDarkTheme ? darkTheme : lightTheme}>
+      <StyleApp>
+        <SunIcon />
+        <Switch isDarkTheme={isDarkTheme} onToggle={onToggle} />
+        <MoonIcon />
+        <Name>Victoria Bogustka</Name>
+        <Info color="red">Username: Alozi</Info>
+        <Info>
+          GitHub:{" "}
+          <Link target="_blank" href="https://github.com/Alozi">
+            https://github.com/Alozi
+          </Link>
+        </Info>
+      </StyleApp>
+    </ThemeProvider>
   );
 }
